9f07061309d20456048c9e3db1f97334b6bb2a80
Marc Ablay <marc.ablay@gmail.com>
2021-04-27 23:36:19 +0200

    NoCol in the browser

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
-'Anti-collision' system, as demonstrated in this [YouTube video](https://www.youtube.com/watch?v=SqpIcsN0FTI)
+# NoColJs
 
-![Picture of various sized balls missing one another](https://user-images.githubusercontent.com/159109/115645659-bc4cd480-a2ee-11eb-885e-ab72cb75205d.png)
+Multi object orbiting simulation converging towards collision free trajectories.
 
-## Usage
+This is a web version of Jean Tampons wonderful [NoCol](https://github.com/johnBuffer/NoCol) project. Check it out, it has a video!
 
-The following keyboard shortcuts are available
+
+## User Interaction
 
 * <kbd>E</kbd> toggle v-sync. V-sync limits the number of frames per second, so disabling it speeds up the simulation.
 * <kbd>A</kbd> toggle the visibility of the traces.
 * <kbd>space</kbd> toggle slow-motion.
 * <kbd>R</kbd> reset the view.
-* <kbd>esc</kbd> close the application.
+* <kbd>mouse-click</kbd> to spawn a new object.
 
-You can zoom into the simulation by scrolling, and move around by dragging with the mouse.
+On a Mac you can pan and scroll around. Not sure if it behaves the same way on Windows and Linux.
---
+++ public/sketch.js
@@ -0,0 +1,64 @@
+import p5js from 'p5js/p5.js/p5'
+import { World } from './world'
+
+function sketch (p5) {
+  const { innerWidth: width, innerHeight: height } = window
+
+  let ballCount = 25
+  let world
+  initWorld()
+
+  let animate = true
+  let vSync = true
+  let step = false
+
+  function initWorld () {
+    world = new World(p5, width, height, ballCount, 0.02)
+  }
+
+  p5.setup = function () {
+    p5.createCanvas(width, height, p5.P2D)
+  }
+
+  p5.draw = function () {
+    if (!animate && !step) return
+    p5.background(`rgba(0, 0, 0, 0.1)`)
+    if (vSync) {
+      world.tick()
+    } else {
+      const next = Date.now() + 32
+      while (Date.now() < next) {
+        world.tick()
+      }
+    }
+    world.render(p5)
+    step = false
+  }
+
+  p5.keyPressed = function (event) {
+    const key = event.key.toLowerCase()
+    if (key === '+') return ballCount *= 2
+    if (key === '-') return ballCount /= 2
+    if (key === ' ') return animate = !animate
+    if (key === 'e') return vSync = !vSync
+    if (key === 'r') return initWorld()
+    if (key === 's') {
+      console.log('step')
+      // vSync = true
+      animate = false
+      step = true
+      return
+    }
+    if (event.key === 'Shift') return
+    console.log('event', event)
+  }
+
+  p5.mouseClicked = function (event) {
+    const x = p5.mouseX
+    const y = p5.mouseY
+    world.addBall(p5, x, y)
+  }
+
+}
+
+const p5 = new p5js(sketch)
